b617e8a651f139978fa95ad121c8d68bc170c75e
akielthy <ack2905@gmail.com>
2016-12-07 20:01:49 +0000

    Updated project thumbnails

--- js/scripts.js
+++ js/scripts.js
@@ -36,3 +36,23 @@ $(document).ready( function() {
 		clickEvent = false;
 	});
 });
+    $( document ).ready(function() {
+
+    $(function () {
+        $('.mm_tooltip').tooltip({
+            selector: "[data-toggle=tooltip]",
+            container: "body"
+        })
+    });
+ 
+    $('.thumbnail').hover(
+        function(){
+            $(this).find('.caption').slideDown(300); //.fadeIn(250)
+        },
+        function(){
+            $(this).find('.caption').slideUp(200); //.fadeOut(205)
+        }
+    ); 
+
+
+});
